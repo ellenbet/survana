665999fdd6dee8021368aa9993feb90f815bfb58
ellenbet-uio <ellenbet@ulrik.uio.no>
2025-09-24 12:05:57 +0200

    removed .vscode

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ How to use and build:
 1. Clone repo
 2. Run ``uv sync`` in terminal
 3. Run ``pre-commit install`` in terminal
-4. Make sure to have
+4. Make sure to have some helpful extensions installed: mypy, isort, black, flake8
+5.
--- config.py
+++ config.py
@@ -1,19 +1,25 @@
-import os
 import logging
-from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 # Displays info messages
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 # Logger - logging.getLogger(__name__) reflects the module
 logger: logging.Logger = logging.getLogger("config")
 
 BASE_DIR: Path = Path(os.getenv("BASE_DIR", "survana"))
-DATA_PATH: Path = BASE_DIR/"data"
-SRC_PATH: Path = BASE_DIR/"src"
+DATA_PATH: Path = BASE_DIR / "data"
+SRC_PATH: Path = BASE_DIR / "src"
 
 paths: list[Path] = [BASE_DIR, DATA_PATH, SRC_PATH]
-logging.info("\nDEFINED PROJECT PATHS:\n" + "\n".join([str(path) for path in paths]))
 
+# Printing config overview
+logging.info(
+    "\nDEFINED PROJECT PATHS:\n" + "\n".join([str(path) for path in paths])
+)
